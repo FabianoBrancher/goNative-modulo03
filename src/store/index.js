e08d0945cac00eb027c34036c983a4142ab32923
Fabiano Brancher <fabianobrancher@gmail.com>
2019-06-26 14:43:56 -0300

    refatorando o código

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,45 @@
 import { createStore } from 'redux';
 
-const INITIAL_STATE = ['Fazer café', 'Estudar react-native', 'entender o redux'];
+const INITIAL_STATE = [
+  { id: 1, text: 'Fazer café', completed: false },
+  { id: 2, text: 'Estudar react-native', completed: true },
+  { id: 3, text: 'entender o redux', completed: false },
+];
 
 // Reducer
 function reducer(state = INITIAL_STATE, action) {
-  if (action.type === 'ADD_TODO') {
-    return [...state, action.text];
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        {
+          id: Math.random(),
+          text: action.text,
+          completed: false,
+        },
+      ];
+    case 'MARK_AS_COMPLETED':
+      return [
+        state.map(item => item.id === action.id),
+        { completed: true },
+      ];
+
+    default:
+      return state;
   }
-  return state;
+
+  // if (action.type === 'ADD_TODO') {
+  //   return [
+  //     ...state,
+  //     {
+  //       id: Math.random(),
+  //       text: action.text,
+  //       completed: false,
+  //     },
+  //   ];
+  // }
+
 }
-
-// Actions
-
-// Adicionar um todo
-
-// Marcar TODO como completo
 
 // Não pode ser criado vazio
 const store = createStore(reducer);
