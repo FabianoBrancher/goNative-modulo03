083cd1fd89f6958492975bd7fb75843cceeb6f53
Fabiano Brancher <fabianobrancher@gmail.com>
2019-06-26 22:46:20 -0300

    pagina de login

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -2,14 +2,49 @@ import React, { Component } from 'react';
 
 import { View, Text } from 'react-native';
 
-// import { Container } from './styles';
+import api from '~/services/api';
+
+import {
+  Container,
+  Input,
+  Button,
+  ButtonText,
+  Error
+} from './styles';
 
 export default class login extends Component {
+  state = {
+    username: '',
+  };
+
+  handleSubmit = async () => {
+    const { username } = this.state;
+    try {
+      await api.get(`/users/${username}`);
+      // deu certo
+
+      // navegar para a rota que mostra os repositorios
+
+    } catch (error) {
+      // seta o erro
+    }
+  }
+
   render() {
+    const { username } = this.state;
     return (
-      <View>
-        <Text> LOGIN </Text>
-      </View>
-    )
+      <Container>
+        <Input
+          value={username}
+          onChangeText={text => this.setState({ username: text })}
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="Digite seu usuário"
+        />
+        <Button onPress={this.handleSubmit}>
+          <ButtonText>Entrar</ButtonText>
+        </Button>
+      </Container>
+    );
   }
 }
